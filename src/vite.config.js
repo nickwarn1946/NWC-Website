d13b745ae18e02638042781c5a443427d4b27df2
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 14:51:12 +0000

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/NWC-Website/', 
+    export default defineConfig({
+  
+  base: '/', 
+  plugins: [react()],
+})
     plugins: [react()],
     build: {
     outDir: 'dist',
