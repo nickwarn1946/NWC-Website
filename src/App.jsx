f56b44c6fc1e32f2b62e3bfa9982912e2e073cd5
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 15:31:29 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Payment from './pages/Payments'; 
+
+const Home = () => (
+  <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+    <h1>Intelligent Agility</h1>
+    <img src="./solutions.jpg" alt="Solutions Graphic" style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }} />
+    <br />
+    <Link to="/payments" style={{ fontSize: '20px', color: 'blue', display: 'inline-block', marginTop: '30px' }}>
+      Book a Session
+    </Link>
+  </div>
+);
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/payments" element={<Payment />} />
+      </Routes>
+    </Router>
+  );
+}
