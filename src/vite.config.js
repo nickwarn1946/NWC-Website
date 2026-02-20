9d2029c119e40b595ff6acb569d510c49a2b90bb
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 09:13:55 +0000

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Tell Vite the name of your GitHub Repository
+  base: '/YOUR-REPOSITORY-NAME/', 
   server: {
     port: 3000,
   }
