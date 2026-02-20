95e028ad8f20f1a268ef92ca206d0182a6ec0a0f
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 10:54:09 +0000

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 1. Tell Vite the name of your GitHub Repository
+  
   base: '/NWC-Website/', 
   server: {
     port: 3000,
