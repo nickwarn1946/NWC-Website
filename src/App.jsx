5867fb5eaae9337d0eeb97138f371e7b0a4ec2eb
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 10:18:17 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// This line below connects your real payment file!
+import Payment from './pages/payments'; 
 
-// Simple Home Component (Your Landing Page)
 const Home = () => (
   <div style={{ textAlign: 'center', padding: '50px' }}>
     <h1>Intelligent Agility</h1>
@@ -11,22 +12,16 @@ const Home = () => (
       style={{ maxWidth: '100%', height: 'auto' }} 
     />
     <br />
-    <a href="/payment" style={{ fontSize: '20px', color: 'blue' }}>Book a Session</a>
-  </div>
-);
-
-// Simple Payment Component (Your Stripe Page)
-const Payment = () => (
-  <div style={{ textAlign: 'center', padding: '50px' }}>
-    <h1>Secure Payment</h1>
-    <p>Connecting to Stripe...</p>
-    {/* Your Stripe Elements code will go here eventually */}
+    {/* We use #/payment to help GitHub find the page easier */}
+    <a href="/payment" style={{ fontSize: '20px', color: 'blue', display: 'block', marginTop: '20px' }}>
+      Book a Session
+    </a>
   </div>
 );
 
 function App() {
   return (
-    <Router>
+    <Router basename="/NWC-Website">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/payment" element={<Payment />} />
