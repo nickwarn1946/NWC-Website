a74f90cb99b8fa4f1a443dba934d75cb7fd4befb
nickwarn1946 <nickwarn1946@gmail.com>
2026-02-20 15:38:54 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import Payment from './pages/Payments';
 const Home = () => (
   <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
     <h1>Intelligent Agility</h1>
-    <img src="./solutions.jpg" alt="Solutions Graphic" style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }} />
+    <img 
+      src="./solutions.jpg" 
+      alt="Solutions Graphic" 
+      style={{ maxWidth: '100%', height: 'auto', marginTop: '20px' }} 
+    />
     <br />
-    <Link to="/payments" style={{ fontSize: '20px', color: 'blue', display: 'inline-block', marginTop: '30px' }}>
+    <Link 
+      to="/payments" 
+      style={{ fontSize: '20px', color: 'blue', display: 'inline-block', marginTop: '30px', textDecoration: 'none', border: '1px solid blue', padding: '10px 20px', borderRadius: '5px' }}
+    >
       Book a Session
     </Link>
   </div>
